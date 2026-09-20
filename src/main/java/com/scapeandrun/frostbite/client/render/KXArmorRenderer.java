b203e3ea655ee88fcee6998fdfb548c7a864d056
LazyToNameThis <flameborneeeee@gmail.com>
2026-09-20 14:49:11 -0700

    uNDO!

--- src/main/java/com/scapeandrun/frostbite/client/render/KXArmorRenderer.java
+++
@@ -1,9 +0,0 @@
-package com.scapeandrun.frostbite.client.render;
-
-import com.scapeandrun.frostbite.client.model.KXArmorModel;
-
-public final class KXArmorRenderer extends PoweredArmorRenderer {
-    public KXArmorRenderer() {
-        super(new KXArmorModel(), 2);
-    }
-}
